919b3105bab6dde7757c057b0250747981e6e815
Brayan346 <braher123456789@gmail.com>
2025-04-03 11:19:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 async function getPokemon() {
     const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
     const url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
-
+    
     try {
         const response = await fetch(url);
         if (!response.ok) throw new Error("Pokémon no encontrado");
         const data = await response.json();
-
+        
         document.getElementById("pokemonInfo").innerHTML = `
             <h2>${data.name.toUpperCase()}</h2>
             <img src="${data.sprites.front_default}" alt="${data.name}">
